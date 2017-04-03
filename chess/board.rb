9fff14b84043c7ce5dd0193d7bc8ec8fa0cc3c47
App Academy Student <student@appacademy.io>
2017-04-03 13:52:17 -0700

    Add board setup and basic movement

---
+++ chess/board.rb
@@ -0,0 +1,50 @@
+require 'byebug'
+require_relative 'pieces/piece.rb'
+
+class Board
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+  end
+
+  def make_starting_grid
+    [0, 1, 6, 7].each do |i|
+      (0..7).each do |j|
+        self[[i,j]] = Piece.new
+      end
+    end
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def []=(from_pos, piece)
+    x, y = from_pos
+    @grid[x][y] = piece
+  end
+
+  def dup
+
+  end
+
+  def move_piece!(from_pos, to_pos)
+    if self[from_pos].nil?
+      raise ArgumentError.new "There is no piece here."
+    end
+
+    unless self[to_pos].nil?
+      raise ArgumentError.new "There is a piece there"
+    end
+
+    self[to_pos] = self[from_pos]
+    self[from_pos] = nil
+  end
+
+  def checkmate?
+
+  end
+
+  def find_king(color)
+  end
+end
---
+++ chess/pieces/piece.rb
@@ -0,0 +1,21 @@
+class Piece
+  def initialize
+  end
+
+  def to_s
+  end
+
+  def empty?
+  end
+
+  def symbol
+  end
+
+  def valid_moves
+  end
+
+  private
+
+  def move_into_checks(to_pos)
+  end
+end
