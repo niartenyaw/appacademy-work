015af7197da1d9c28998854a0510ae80546f7d6d
Aaron Wayne <amwayne90@gmail.com>
2017-04-05 14:23:01 -0700

    Finished towers of hanoi

--- towers_of_hanoi/lib/towers.rb
+++ towers_of_hanoi/lib/towers.rb
@@ -1,8 +1,55 @@
 
 class Towers
+  attr_reader :towers
 
   def initialize(size)
-    
+    @size = size
+    @towers = Array.new(3) { Array.new }
+    (1..size).to_a.reverse.each do |i|
+      @towers[0] << i
+    end
+  end
+
+  def move(start_tower, end_tower)
+    raise "No disks on that tower" if @towers[start_tower].empty?
+    start_disk = @towers[start_tower].last
+    end_disk = @towers[end_tower].last
+    raise "That disk is too large" if end_disk && start_disk > end_disk
+
+    @towers[end_tower] << @towers[start_tower].pop
+  end
+
+  def won?
+    (@towers[1].length == @size) || (@towers[2].length == @size)
+  end
+
+  def prompt
+    puts "Enter two towers:"
+    gets.chomp.split(",").map(&:to_i)
+  end
+
+  def render
+    system("clear")
+    @towers.each_with_index do |row, i|
+      puts "#{i} #{row.join(" ")}"
+    end
+  end
+
+  def play
+    render
+    until won?
+      begin
+        move = prompt
+        move(move[0], move[1])
+        render
+      rescue => e
+        puts e.message
+      end
+    end
+    puts "won!"
   end
 
 end
+
+#a = Towers.new(3)
+#a.play
--- towers_of_hanoi/spec/towers_spec.rb
+++ towers_of_hanoi/spec/towers_spec.rb
@@ -3,9 +3,10 @@ require 'towers'
 
 describe Towers do
 
+  let(:size) { 3 }
+  subject(:hanoi) { Towers.new(size) }
+
   describe '#initialize' do
-    let(:size) { 3 }
-    subject(:hanoi) { Towers.new(size) }
 
     it 'sets three towers' do
       expect(hanoi.towers.length).to eq(3)
@@ -21,4 +22,86 @@ describe Towers do
     end
   end
 
+  describe '#move' do
+
+    it 'raises error when start tower is empty' do
+      expect { hanoi.move(2, 0) }.to raise_error("No disks on that tower")
+    end
+
+    context 'when moving to empty end tower' do
+      before(:each) { hanoi.move(0, 1) }
+      it 'removes top disc from start tower' do
+        expect(hanoi.towers[0]).to eq([3, 2])
+      end
+      it 'puts top disc onto end tower' do
+        expect(hanoi.towers[1]).to eq([1])
+      end
+      it 'does not change other tower' do
+        expect(hanoi.towers[2]).to be_empty
+      end
+
+    end
+
+    context 'when end tower has disc' do
+      before(:each) do
+        hanoi.move(0, 1)
+        hanoi.move(0, 2)
+      end
+
+      it 'moves top disc to tower with larger disc on it' do
+        hanoi.move(1, 2)
+        expect(hanoi.towers).to eq(
+          [
+            [3],
+            [],
+            [2, 1]
+          ]
+        )
+      end
+
+      it 'raises an error if end tower has smaller disc on it' do
+        expect { hanoi.move(0,1) }.to raise_error("That disk is too large")
+      end
+    end
+  end
+
+
+  describe '#won?' do
+    it 'does not return true when tower 0 is full' do
+      expect(hanoi.won?).to be false
+    end
+
+    it 'does not return true when the disks are not on one tower' do
+      hanoi.move(0, 2)
+
+      expect(hanoi.won?).to be false
+    end
+
+    context 'when all disks are on one tower' do
+
+      it 'returns true for tower 1' do
+        hanoi.move(0, 1)
+        hanoi.move(0, 2)
+        hanoi.move(1, 2)
+        hanoi.move(0, 1)
+        hanoi.move(2, 0)
+        hanoi.move(2, 1)
+        hanoi.move(0, 1)
+
+        expect(hanoi.won?).to be true
+      end
+
+      it 'returns true for tower 2' do
+        hanoi.move(0, 2)
+        hanoi.move(0, 1)
+        hanoi.move(2, 1)
+        hanoi.move(0, 2)
+        hanoi.move(1, 0)
+        hanoi.move(1, 2)
+        hanoi.move(0, 2)
+
+        expect(hanoi.won?).to be true
+      end
+    end
+  end
 end
